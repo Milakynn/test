e289046e7fb0de7872e747e5aa89cc35a79161e8
Milakynn <lindyrella.k@gmail.com>
2024-10-10 02:18:10 +0000

    Responsive

---
+++ index.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "main-navigation") {
+      x.className += " responsive";
+    } else {
+      x.className = "main-navigation";
+    }
+}
