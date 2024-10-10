a6b0ffd0759e4cdc3d9dcc6def4a9cdd9d9e1f2b
Milakynn <lindyrella.k@gmail.com>
2024-10-10 03:09:55 +0000

    changes

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "main-navigation") {
-      x.className += " responsive";
-    } else {
-      x.className = "main-navigation";
+// function myFunction() {
+//     var x = document.getElementById("myTopnav");
+//     if (x.className === "main-navigation") {
+//       x.className += " responsive";
+//     } else {
+//       x.className = "main-navigation";
+//     }
+// }
+
+window.addEventListener('load' , () => {
+  const btn = document.querySelector('.icon')
+  const nav = document.querySelector('.main-navigation')
+  
+  btn.addEventListener('click', () => {
+    if( nav.classList.contains('open') ) {
+      nav.classList.remove('open')
     }
-}
+    else{
+      nav.classList.add('open')
+    }
+  })
+})
